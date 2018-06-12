c11740589c587da5e10b078750606633635e7e8a
duxiaopan <364499854@qq.com>
2018-06-12 23:44:19 +0800

    初次导入 1、支持ListView、GridView、RecycleView下拉刷新上拉加载

---
+++ app/src/main/java/com/dxp/utils/ViewUtils.java
@@ -0,0 +1,39 @@
+package com.dxp.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.util.Log;
+import android.view.View;
+
+public class ViewUtils {
+
+    public static void showLocation(View view){
+        int[] location = new int[2];
+        view.getLocationInWindow(location);
+        Log.e("----",String.valueOf(location[0]+"--"+location[1])+"--"+view.getMeasuredHeight()+"--"+view.getMeasuredWidth());
+    }
+
+    /**
+     * 选择变换
+     *
+     * @param origin 原图
+     * @param alpha  旋转角度，可正可负
+     * @return 旋转后的图片
+     */
+    public static Bitmap rotateBitmap(Bitmap origin, float alpha) {
+        if (origin == null) {
+            return null;
+        }
+        int width = origin.getWidth();
+        int height = origin.getHeight();
+        Matrix matrix = new Matrix();
+        matrix.setRotate(alpha);
+        // 围绕原地进行旋转
+        Bitmap newBM = Bitmap.createBitmap(origin, 0, 0, width, height, matrix, false);
+        if (newBM.equals(origin)) {
+            return newBM;
+        }
+        origin.recycle();
+        return newBM;
+    }
+}
